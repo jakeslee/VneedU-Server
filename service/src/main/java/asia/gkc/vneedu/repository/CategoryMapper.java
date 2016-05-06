582939292661421ae0a136cf34a1b33eaefbda0e
Jakes Lee <jakeslee66@gmail.com>
2016-05-06 10:58:14 +0800

    add new selection method for category

--- service/src/main/java/asia/gkc/vneedu/repository/CategoryMapper.java
+++ service/src/main/java/asia/gkc/vneedu/repository/CategoryMapper.java
@@ -4,4 +4,5 @@ import asia.gkc.vneedu.model.Category;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface CategoryMapper extends Mapper<Category> {
+    Category getCategoryByType(String type);
 }
