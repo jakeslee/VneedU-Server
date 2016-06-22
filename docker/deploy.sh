1bfd1297189aca297f5c4ceee3c1467f1bb59868
Jakes Lee <jakeslee66@gmail.com>
2016-06-22 17:56:27 +0800

    fix provider ip error

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,10 +2,11 @@
 
 set -a
 
+REGISTRY_ADDR=10.5.0.254:2181
+
 if [[ "$1" = "provider" ]]; then
-    echo "Deploying Provider"
-    PROVIDER_HOST=`hostname -I | awk '{print $1}'`
-    docker-compose up --build
+    echo "Deploying Provider" \
+    && PROVIDER_HOST=`hostname -I | awk '{print $1}'` \
+    && docker-compose build \
+    && docker-compose run provider
 fi
-
-
--- docker/producer/build-project.sh
+++ docker/producer/build-project.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -e
-cd VneedU-Server
+pwd
+cd /app/VneedU-Server
 
 function UPDATE_CODE() {
 	git stash &&\
@@ -9,10 +10,14 @@ function UPDATE_CODE() {
 }
 
 UPDATE_CODE
-VERSION=$(cat build.gradle | awk '$1 ~ /version/{print $2}' | sed -e "s/^'//" -e "s/'$//") \
+VERSION=$(cat build.gradle | awk '$1 ~ /version/{print $2}' | sed -e "s/^'//" -e "s/'$//")
 
-&& ./gradlew :vneedu-service:assembleDist \
-&& ./gradlew :vneedu-server:war \
-&& unzip ./service/build/distributions/vneedu-service-$VERSION".zip" -d /app/source \
-&& mv -Tf /app/source/vneedu-service-$VERSION /app/provider \
-&& cp ./server/build/libs/vneedu-server-$VERSION".war" $TOMCAT_WEBAPPS
+gradle :vneedu-service:assembleDist :vneedu-server:war \
+&& unzip -o ./service/build/distributions/vneedu-service-$VERSION".zip" -d /app/source \
+&& ls /app/source \
+&& rsync -a --delete /app/source/vneedu-service-$VERSION/ /app/provider \
+&& cp -f ./server/build/libs/vneedu-server-$VERSION".war" $TOMCAT_WEBAPPS
+
+cat /etc/hosts | sed "s/.*$(hostname)/${PROVIDER_HOST} $(hostname)/" > /etc/hosts
+
+exec "$@"
